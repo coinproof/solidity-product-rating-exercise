5d64a70875d9a0e580a7f40a1ba4b27800973649
Alexandre Justino <alexandretok@gmail.com>
2018-08-06 16:07:02 -0300

    Improve negative tests using assert correctly

--- test/Rating.test.js
+++ test/Rating.test.js
@@ -2,8 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const provider = ganache.provider({gasLimit: '10000000'});
 const Web3 = require('web3');
-const provider = ganache.provider({gasLimit: '100000000000000'});
+// const HDWalletProvider = require('truffle-hdwallet-provider');
+// const provider = new HDWalletProvider(
+//   'know find fiction duty chalk spend until old similar wave garment urban',
+//   'http://127.0.0.1:7545',
+//   0, 5
+// );
 const web3 = new Web3(provider);
 
 // Fetch contract source
@@ -26,22 +32,17 @@ beforeEach(async () => {
 });
 
 describe('Rating', async () => {
-  it('deploys the contract', () => {
-    assert.ok(rating);
-  });
-
   it('adds a product', async () => {
     await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
   });
 
   it('adds a product using different account', async () => {
-    let err;
     try{
       await rating.methods.addProduct(1, "Product 1").send({from: accounts[1], gas: '1000000'});
+      assert(false);
     } catch(_err) {
-      err = _err;
+      assert(_err);
     };
-    assert.ok(err);
   });
 
   it('adds a product that already exists', async () => {
@@ -49,26 +50,28 @@ describe('Rating', async () => {
     let err;
     try{
       await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
+      assert(false);
     } catch(_err) {
-      err = _err;
+      assert(_err);
     };
-    assert.ok(err);
   });
 
   it('adds a product without title', async () => {
     let err;
     try{
       await rating.methods.addProduct(1, "").send({from: accounts[0], gas: '1000000'});
+      assert(false);
     } catch(_err) {
-      err = _err;
+      assert(_err);
     };
-    assert.ok(err);
   });
 
   it('gets the products ids list', async () => {
     await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
     let productIds = await rating.methods.getProductsIds().call({gas: '1000000'});
-    assert.ok(productIds.length && productIds[0] == 1);
+    // console.log(productIds);
+    assert.equal(1, productIds.length);
+    assert.equal(1, productIds[0]);
   });
 
   it('makes a review', async () => {
@@ -79,42 +82,40 @@ describe('Rating', async () => {
   it('makes a review for the same product', async () => {
     await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
     await rating.methods.addReview(1, 4).send({from: accounts[1], gas: '1000000'});
-    let err;
     try{
       await rating.methods.addReview(1, 1).send({from: accounts[1], gas: '1000000'});
+      assert(false);
     } catch(_err){
-      err = _err;
+      assert(_err);
     }
-    assert.ok(err);
   });
 
   it('makes a review with rating out of range', async () => {
     await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
-    let err;
     try{
       await rating.methods.addReview(1, 7).send({from: accounts[1], gas: '1000000'});
+      assert(false);
     } catch(_err){
-      err = _err;
+      assert(_err);
     }
-    assert.ok(err);
   });
 
   it('makes a review for product that doesnt exist', async () => {
-    let err;
     try{
       await rating.methods.addReview(1, 2).send({from: accounts[1], gas: '1000000'});
+      assert(false);
     } catch(_err){
-      err = _err;
+      assert(_err);
     }
-    assert.ok(err);
   });
-
-  let product;
 
   it('gets a specific product', async () => {
     await rating.methods.addProduct(1, "Product 1").send({from: accounts[0], gas: '1000000'});
     await rating.methods.addReview(1, 2).send({from: accounts[1], gas: '1000000'});
-    product = await rating.methods.getProduct(1).call({gas: '1000000'});
-    assert.ok(product.id == 1 && product.title == "Product 1" && product.reviewers[0] == accounts[1] && product.ratings[0] == 2);
+    let product = await rating.methods.getProduct(1).call({gas: '1000000'});
+    assert.equal(1, product.id);
+    assert.equal("Product 1", product.title);
+    assert.equal(accounts[1], product.reviewers[0]);
+    assert.equal(2, product.ratings[0]);
   });
 });
